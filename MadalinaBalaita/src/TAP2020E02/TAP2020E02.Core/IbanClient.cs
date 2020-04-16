40159dfd638cc05f3fa8bbf2ef4c85342ab1ae90
madalinabalaita <madalinabalaita14@gmail.com>
2020-04-16 19:48:19 +0300

    Tried to implement the test

---
+++ MadalinaBalaita/src/TAP2020E02/TAP2020E02.Core/IbanClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TAP2020E02.Core
+{
+    public class IbanClient
+    {
+        public long Iban { get; set; }
+      
+    }
+}
--- MadalinaBalaita/src/TAP2020E02/TAP2020E02.Test/Ib.cs
+++ MadalinaBalaita/src/TAP2020E02/TAP2020E02.Test/Ib.cs
@@ -1,14 +1,37 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TAP2020E02.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
 namespace TAP2020E02.Test
 {
     [TestClass]
-    public class Ib
+    public class IsIbanPalindrom
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
+        public IbanClient client { get; set; }
 
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            client.Iban = 1131007593840000;
+        }
+       
+        [TestMethod]
+        public void IsPalindromThrow()
+        {
+            //Arrange
+            bool isit;
+            bool exceptionThrown = true;
+            //Act
+            try
+            { 
+                isit=Palindrom.IsPalindrom(client.Iban);
+            }
+            catch (InvalidOperationException)
+            {
+                exceptionThrown = true;
+            }
+            //Assert
+            Assert.IsTrue(exceptionThrown);
         }
     }
 }
