1050e43158b073ad3d5e551bff528232168876b4
MaxinoiuMihaiRoberto <56999082+MaxinoiuMihaiRoberto@users.noreply.github.com>
2020-04-16 19:47:45 +0300

    Add files via upload

---
+++ MaxinoiuMihaiRobertoE2/IsPalindrome.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaxinoiuMihaiRobertoE2
+{
+    public class PalindromeExample
+    {
+        public int Id;
+
+        public void IsPalindrome(string IBAN) {
+            string revs="";
+            for (int i = IBAN.Length - 1; i >= 0; i--)  
+            {
+                revs += IBAN[i].ToString();
+            }
+            if (revs == IBAN)  
+            {
+                Console.WriteLine("Your IBAN is palindrome", IBAN, revs);
+            }
+            else
+            {
+                Console.WriteLine("Your IBAN is palindrome", IBAN, revs);
+            }
+            
+        }
+
+        PalindromeExample(int Id) {
+            this.Id = Id;
+        }
+    }
+}
