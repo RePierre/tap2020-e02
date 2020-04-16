d0c8668741d9a8754598f2a7291700b84a9a3573
madalinabalaita <madalinabalaita14@gmail.com>
2020-04-16 19:27:35 +0300

    created test project

---
+++ MadalinaBalaita/src/TAP2020E02/TAP2020E02.Test/Ib.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TAP2020E02.Core;
+namespace TAP2020E02.Test
+{
+    [TestClass]
+    public class Ib
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
