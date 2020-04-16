451b0620a8f1293c0a7b41fc74937c2f635e7213
GalbinitaSebastian97 <61518348+GalbinitaSebastian97@users.noreply.github.com>
2020-04-16 20:05:53 +0300

    Add files via upload

---
+++ GalbinitaSebastian/src/GalbinitaSebastian/IBAN.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GalbinitaSebastian
+{
+    class IBAN
+    {
+        public int Id { get; set; }
+        public long Iban { get; set; }
+
+        public void setIban(long creditCard)
+        {
+            Iban = creditCard;
+        }
+    }
+}
