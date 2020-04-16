a681a45d4af2b9b846a0a84655b143aefa30f39b
Crina-Otilia <purice.crina@yahoo.com>
2020-04-16 18:13:59 +0300

    create context

---
+++ Solution/tap20-e2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tap20_e2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
