11619bdb36412e7880562baca82dbc2fff2c7569
madalinabalaita <madalinabalaita14@gmail.com>
2020-04-16 19:20:41 +0300

    created method to see if an iban is palindorm or not

--- MadalinaBalaita/src/TAP2020E02/TAP2020E02.Core/Palindrom.cs
+++ MadalinaBalaita/src/TAP2020E02/TAP2020E02.Core/Palindrom.cs
@@ -4,7 +4,26 @@ using System.Text;
 
 namespace TAP2020E02.Core
 {
-    class Palindrom
+    public static class Palindrom
     {
+        
+            public static bool IsPalindrom(long Iban)
+            {//methos to see if it is Palindrom or not
+            long n,num,digit,rev=0;
+            num = Iban;
+            n = num;
+            do
+            {
+                digit =num % 10;
+                rev = (rev * 10) + digit;
+                num = num / 10;
+            } while (num != 0);
+
+            if (n == rev)
+                return true;
+            else  return false;
+        }
+       
+        
     }
 }
---
+++ MadalinaBalaita/src/TAP2020E02/TAP2020E02/Iban.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TAP2020E02.Core;
+namespace TAP2020E02
+{
+    public class Iban
+    {
+        [TestClass]
+        public class TestPalindrom
+        {
+            public Iban iban { get; set; }
+            [TestInitialize]
+
+            public void TestInitialize()
+            {
+                iban=new 
+            }
+        }
+    }
+}
