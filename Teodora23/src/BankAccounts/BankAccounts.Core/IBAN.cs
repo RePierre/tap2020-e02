c065df3e950c4066a68429c1354b0edec7650555
Teodora23 <47943153+Teodora23@users.noreply.github.com>
2020-04-16 18:54:02 +0300

    Added IBAN class

---
+++ Teodora23/src/BankAccounts/BankAccounts.Core/IBAN.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BankAccounts.Core
+{
+    public class IBAN
+    {
+        public string Country { get; set; }
+        public string Checksum { get; set; }
+        public string Swift { get; set; }
+        public string AccountNumber { get; set; }
+
+        public IBAN(string iban)
+        {
+            this.Country = iban.Substring(0, 2);
+            this.Checksum = iban.Substring(2, 2);
+            this.Swift = iban.Substring(4, 4);
+            this.AccountNumber = iban.Substring(8);
+        }
+    }
+}
