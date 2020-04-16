99c5928aee794e816c02790f874732d668afdfa0
Iacob Eduard <kaeltuzah98@gmail.com>
2020-04-16 19:55:55 +0300

    Test2

---
+++ mkdir IacobEd/src/TAP2020/TAP2020/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TAP;
+
+namespace TAP2020
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            bool status;
+            string iban;
+
+            Console.WriteLine("Enter your IBAN : ");
+            iban = Console.ReadLine();
+            status = TAP.IsPalindrome.getPalindrome(iban);
+
+            if (status)
+            {
+                Console.WriteLine("It's not a palindrome !");
+            }
+            else
+            {
+                Console.WriteLine("It's palindrome !");
+            }
+            
+        }
+    }
+}
